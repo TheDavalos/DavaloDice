2113051c5729714110204ca294f7df1b06fe9f1f
Jeff Davis <103478952+TheDavalos@users.noreply.github.com>
2026-02-14 11:40:17 -0500

    Add three.js and cannon-es dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import PhysicsDice from '../lab/PhysicsDice'
 import '../lab/PhysicsDice.scss'
 
 function App() {
-  return (
-    <div className="App">
-      <h1>DavaloDice - Physics Dice Animations</h1>
-      <PhysicsDice />
-    </div>
-  )
+    return (
+        <div className="App">
+            <h1>DavaloDice - Physics Dice Animations</h1>
+            <PhysicsDice />
+        </div>
+    )
 }
 
 export default App
