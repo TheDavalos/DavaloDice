cfb303700c409c5f42855999766ddff78ad000fb
Jeff Davis <103478952+TheDavalos@users.noreply.github.com>
2026-02-14 11:36:26 -0500

    Add Vite build configuration and Netlify setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PhysicsDice from '../lab/PhysicsDice'
+import '../lab/PhysicsDice.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>DavaloDice - Physics Dice Animations</h1>
+      <PhysicsDice />
+    </div>
+  )
+}
+
+export default App
